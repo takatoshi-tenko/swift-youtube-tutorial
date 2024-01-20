52a4582573c4c9b610ecd18b7b98a316f65a9698
takatoshi.tenko <chanten811@gmail.com>
2024-01-21 00:31:20 +0900

    vstack, hstack, zstackの練習

--- swift-youtube/ContentView.swift
+++ swift-youtube/ContentView.swift
@@ -10,12 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 300, height: 50)
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 50)
+            
+            HStack {
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: 100, height: 100)
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: 70, height: 70)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 40, height: 40)
+            }
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.green)
+                    .frame(width: 300, height: 200)
+                VStack {
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(.red)
+                            .frame(width: 100, height:50)
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(width: 100, height:50)
+                    }
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 100, height: 50)
+                        Rectangle()
+                            .foregroundColor(.yellow)
+                            .frame(width: 100, height:50)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
