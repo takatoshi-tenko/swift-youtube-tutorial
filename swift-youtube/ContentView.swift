f31c35c8072fcbea7c39631bedcdb148a65dd1b8
takatoshi.tenko <chanten811@gmail.com>
2024-01-20 19:07:00 +0900

    Initial Commit

---
+++ swift-youtube/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-youtube
+//
+//  Created by 天孝高俊 on 2024/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-youtube/swift_youtubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_youtubeApp.swift
+//  swift-youtube
+//
+//  Created by 天孝高俊 on 2024/01/20.
+//
+
+import SwiftUI
+
+@main
+struct swift_youtubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
