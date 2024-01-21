273bb0d0f899741f734d7b5321ffeac9d15fd38a
takatoshi.tenko <chanten811@gmail.com>
2024-01-21 14:08:10 +0900

    オブジェクト生成、メソッド使用の練習

--- swift-youtube/ContentView.swift
+++ swift-youtube/ContentView.swift
@@ -8,50 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputText = ""
+    @State var isOn = true
     var body: some View {
         VStack {
-            Rectangle()
-                .foregroundColor(.red)
-                .frame(width: 300, height: 50)
-            Rectangle()
-                .foregroundColor(.blue)
-                .frame(width: 300, height: 50)
-            
-            HStack {
-                Rectangle()
-                    .foregroundColor(.blue)
-                    .frame(width: 100, height: 100)
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 70, height: 70)
-                Rectangle()
-                    .foregroundColor(.yellow)
-                    .frame(width: 40, height: 40)
-            }
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.green)
-                    .frame(width: 300, height: 200)
-                VStack {
-                    HStack {
-                        Rectangle()
-                            .foregroundColor(.red)
-                            .frame(width: 100, height:50)
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .frame(width: 100, height:50)
-                    }
-                    HStack {
-                        Rectangle()
-                            .foregroundColor(.black)
-                            .frame(width: 100, height: 50)
-                        Rectangle()
-                            .foregroundColor(.yellow)
-                            .frame(width: 100, height:50)
-                    }
-                }
-            }
+            TextField("ここに文字を入力してください", text: $inputText)
+            Toggle("スイッチ", isOn: $isOn)
         }
+        .padding()
     }
 }
 
